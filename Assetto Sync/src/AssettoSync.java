72fa0e29d7025a9457cb039be035a609f4786a0b
Burns Hudson <burnstheman@gmail.com>
2015-02-24 22:39:49 -0500

    Ported current Car filesystem and checksum code to Track code and added test code in main to make sure it all works

--- Assetto Sync/src/AssettoSync.java
+++ Assetto Sync/src/AssettoSync.java
@@ -8,9 +8,12 @@ public class AssettoSync {
 	
 	 
 	
-	public static String assettoRootFolder = "E:\\Steam\\steamapps\\common\\assettocorsa";
+	public static String assettoRootFolder;
 	
 	public static void main(String[] args) {
+		assettoRootFolder = "E:\\Steam\\steamapps\\common\\assettocorsa";
+		
+		
 		System.out.println(System.getProperty("user.dir"));
 		System.out.println("Getting car list...");
 		carList = setCarList();
@@ -36,6 +39,31 @@ public class AssettoSync {
 			}
 		    System.out.println();
 		}
+		
+		System.out.println("Getting track list...");
+		trackList = setTrackList();
+		System.out.println("Done.");
+		
+		
+		
+		Track tempTrack = getTrackByName("blackwood");
+		
+		System.out.println("Loading track folder checksum....");
+		System.out.println("   Track folder checksum: " + tempTrack.getTotalChecksum());
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Individual file checksums:");
+		for (SingleFile file : tempTrack.getFileList()) {   
+		    try {
+				System.out.println(file.getLocalFilepath()+ " " + file.getFilesize()/1024 +"kb");
+				System.out.println("  checksum: "+ file.getmd5Checksum());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    System.out.println();
+		}
 	}
 	
 	
@@ -46,6 +74,18 @@ public class AssettoSync {
 	        if(carList.get(i).getName().equalsIgnoreCase(name)){
 	        	System.out.println("Found car.");
 	            return carList.get(i);
+	        }
+		}
+		return null;
+	}
+	
+	private static Track getTrackByName(String name){
+		System.out.println("Searching for track: " + name);
+		
+		for(int i=0;i<trackList.size();i++){
+	        if(trackList.get(i).getName().equalsIgnoreCase(name)){
+	        	System.out.println("Found track.");
+	            return trackList.get(i);
 	        }
 		}
 		return null;
@@ -69,31 +109,46 @@ public class AssettoSync {
 		return carListTemp;
 	}
 	
-	public static void getTrackList() {
+	private static ArrayList<Track> setTrackList() {
+		ArrayList<Track> trackListTemp = new ArrayList<>();
 		
-	}
-	
-	public static void getDirectoryContents(File dir)
-	{
-		
-	}
-	
-	public static void displayDirectoryContents(File dir, int curDepth) {
-		try {
-			File[] files = dir.listFiles();
-			for (File file : files) {
-				if (file.isDirectory()) {
-					System.out.println(new String(new char[curDepth]).replace("\0", "    ") +
-							"directory:" + file.getCanonicalPath());
-					displayDirectoryContents(file,curDepth+1);
-				} else {
-					System.out.println(new String(new char[curDepth]).replace("\0", "    ") +
-								"    file:" + file.getCanonicalPath());
-				}
+		File dir = new File(assettoRootFolder + "/content/tracks");
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				Track temp = new Track(file.getName(), file.getPath());
+				trackListTemp.add(temp);
+			} else {
+				//Item found not in expected track format, ignore.
+				//TODO: Maybe handle this?
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		return trackListTemp;
 	}
+	
+	
+	/*
+	* This is recursive directory searching based on 
+	* http://www.avajava.com/tutorials/lessons/how-do-i-recursively-display-all-files-and-directories-in-a-directory.html
+	* This is referenced in other directory searching functions in my code.
+	*
+	*public static void displayDirectoryContents(File dir, int curDepth) {
+	*	try {
+	*		File[] files = dir.listFiles();
+	*		for (File file : files) {
+	*			if (file.isDirectory()) {
+	*				System.out.println(new String(new char[curDepth]).replace("\0", "    ") +
+	*						"directory:" + file.getCanonicalPath());
+	*				displayDirectoryContents(file,curDepth+1);
+	*			} else {
+	*				System.out.println(new String(new char[curDepth]).replace("\0", "    ") +
+	*							"    file:" + file.getCanonicalPath());
+	*			}
+	*		}
+	*	} catch (IOException e) {
+	*		e.printStackTrace();
+	*	}
+	*}
+	*/
 	
 }
--- Assetto Sync/src/Track.java
+++ Assetto Sync/src/Track.java
@@ -1,4 +1,73 @@
+import java.io.File;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 
 public class Track {
-
+	private String name;
+	private String path;
+	
+	private ArrayList<SingleFile> fileList = new ArrayList<>();;
+	
+	public Track(String trackName, String filePath)
+	{
+		name = trackName;
+		path = filePath;
+	}
+	
+	private void setFileList(File dir){
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				setFileList(file);
+			} else {
+				fileList.add(new SingleFile(file.getName(), file.getPath()));
+			}
+		}
+	}
+	
+	
+	public String getTotalChecksum(){
+		
+		BigInteger runningValue = null;
+		for (SingleFile file : getFileList()) {
+			if(runningValue == null)
+			{
+				try {
+					runningValue = new BigInteger(file.getmd5Checksum(),16);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else
+			{
+				try {
+					runningValue = runningValue.add(new BigInteger(file.getmd5Checksum(),16)); 
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return runningValue.toString(16);
+		
+	}
+	
+	public ArrayList<SingleFile> getFileList(){
+		if(fileList.size() == 0)
+		{
+			File dir = new File(path);
+			setFileList(dir);
+		}
+		return fileList;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getPath(){
+		return path;
+	}
 }
